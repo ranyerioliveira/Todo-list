29c5310ad21e5ea99e8a1526a88c96f1a40d0d46
ranyerioliveira <101151286+ranyerioliveira@users.noreply.github.com>
2023-02-28 13:47:28 -0300

    Todo list finalizado
    Começando o localstorage

--- todoList.js
+++ todoList.js
@@ -4,12 +4,16 @@ var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 
 var buttonElement = document.querySelector('#app button');
+//buscando a lista convertida em JSON, o || é uma array vazia para caso aconteça erro de lista vazia
+var todos = JSON.parse(localStorage.getItem('lista_todos')) || [];
 
 //array com a lista de itens pré cadastrados
-var todos = ['Arrumar a casa', 'Estudar estacio', 'Estudar Anhanguera', 'Estudar JavaScript'];
+//retirar a lista todos com as arrays pq foi converitada na json
+//var todos = ['Arrumar a casa', 'Estudar estacio', 'Estudar Anhanguera', 'Estudar JavaScript'];
+
+
 
 //renderizar a lista na tela
-
 function renderTodos(){
     //limpar a alista do todos
 
@@ -24,7 +28,10 @@ function renderTodos(){
         //criado a tag <a> com um href passando '#'
         var linkElement = document.createElement('a');
         linkElement.setAttribute('href', '#');
-        var linkText = document.createTextNode('Excluir');
+        //parte da funcinalidade do botão de excluir
+        var posicao = todos.indexOf(todo);
+        linkElement.setAttribute('onclick', 'deletarTodo('+posicao+')');
+        var linkText = document.createTextNode(' - Excluir');
         linkElement.appendChild(linkText);
         
         //aparecer na tela
@@ -50,13 +57,23 @@ function adicionarTodo(){
     //limpar o texto que ta no imput
     inputElement.value='';
     renderTodos();
-
+    salvarDados()
 }
 
 buttonElement.onclick = adicionarTodo;
 
 //dando função ao botão excluir
 
-function deletarTodo(){
+function deletarTodo(posicao){
+    //splice remove algo da lista passado a posição e o tem do array
+    todos.splice(posicao,1)
+    renderTodos();
+    salvarDados()
     
 }
+ //Criar uma função para salvar os dados no localStorage
+
+ function salvarDados(){
+    //invês de passar o a chave (todos) passara convertendo a lista com JSON
+    localStorage.setItem('lista_todos', JSON.stringify(todos));
+ }
